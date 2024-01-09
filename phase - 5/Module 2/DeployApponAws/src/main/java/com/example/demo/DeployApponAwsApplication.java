4306afab0b87c0651713219d255d43da445f447b
Ullashp03 <ullashp40@gmail.com>
2024-01-09 21:21:04 +0530

    practice project

--- phase - 5/Module 2/DeployApponAws/src/main/java/com/example/demo/DeployApponAwsApplication.java
+++ phase - 5/Module 2/DeployApponAws/src/main/java/com/example/demo/DeployApponAwsApplication.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
+@SpringBootApplication
 public class DeployApponAwsApplication {
-@ResponseBody
-	@RequestMapping("/")
-	public String demo() {
-		return "Hi Leaners welcome to AWS";
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployApponAwsApplication.class, args);
 	}
+
 }
